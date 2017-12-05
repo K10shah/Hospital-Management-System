1b84d993bc223c0f53f780bc9296ec06a16a4a7d
ketan <ketansha@buffalo.edu>
2017-12-05 16:01:58 -0500

    New changes

--- Hospital_Management_Version2/src/common/Login.java
+++ Hospital_Management_Version2/src/common/Login.java
@@ -3,18 +3,39 @@ import java.util.*;
 
 public class Login {
 
+	//Usernames and passwords
 	private Hashtable<String, String> loginData = new Hashtable<String, String>();
+
+	//list of all the users of the system mapping to their roles(doctors, admin, patient, etc)
+	public static Hashtable<String, String> users = new Hashtable<String, String>();
+
+	//list of doctors
+	public static Hashtable<String, Doctor> doctors = new Hashtable<String, Doctor>();
+
+	//list of patients
+	public static Hashtable<String, Patient> patients = new Hashtable<String, Patient>();
+
+	//list of receptionists
+	public static Hashtable<String, Receptionist> receps= new Hashtable<String, Receptionist>();
+
 
 	//getter for loginData
 	public Hashtable<String, String> getLoginData()
 	{
 		return this.loginData;
 	}
+	
+	public String getRole(String username)
+	{
+		return users.get(username);
+	}
+	
 	public Login()
 	{
 		//Default admin user so that atleast the admin can login
 		//This is only for the demo purpose
 		loginData.put("admin", "admin");
+		users.put("admin","admin");
 	}
 
 
