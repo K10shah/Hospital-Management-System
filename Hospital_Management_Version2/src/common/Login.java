117952124a0c833b0c66800236343f198d626235
ketan <ketansha@buffalo.edu>
2017-12-05 18:48:59 -0500

    Admin page now had add and delete user capability

--- Hospital_Management_Version2/src/common/Login.java
+++ Hospital_Management_Version2/src/common/Login.java
@@ -7,16 +7,16 @@ public class Login {
 	private Hashtable<String, String> loginData = new Hashtable<String, String>();
 
 	//list of all the users of the system mapping to their roles(doctors, admin, patient, etc)
-	public static Hashtable<String, String> users = new Hashtable<String, String>();
+	private static Hashtable<String, String> users = new Hashtable<String, String>();
 
 	//list of doctors
-	public static Hashtable<String, Doctor> doctors = new Hashtable<String, Doctor>();
+	private static Hashtable<String, Doctor> doctors = new Hashtable<String, Doctor>();
 
 	//list of patients
-	public static Hashtable<String, Patient> patients = new Hashtable<String, Patient>();
+	private static Hashtable<String, Patient> patients = new Hashtable<String, Patient>();
 
 	//list of receptionists
-	public static Hashtable<String, Receptionist> receps= new Hashtable<String, Receptionist>();
+	private static Hashtable<String, Receptionist> receps= new Hashtable<String, Receptionist>();
 
 
 	//getter for loginData
@@ -24,12 +24,31 @@ public class Login {
 	{
 		return this.loginData;
 	}
-	
+
+	public Hashtable<String, String> getUsers()
+	{
+		return this.users;
+	}
+
+	public Hashtable<String, Doctor> getDoctors()
+	{
+		return this.doctors;
+	}
+
+	public Hashtable<String, Patient> getPatients()
+	{
+		return this.patients;
+	}
+
+	public Hashtable<String, Receptionist> getRecep()
+	{
+		return this.receps;
+	}
 	public String getRole(String username)
 	{
 		return users.get(username);
 	}
-	
+
 	public Login()
 	{
 		//Default admin user so that atleast the admin can login
@@ -40,11 +59,44 @@ public class Login {
 
 
 	//Function for adding new user
-	public void addNewUser(String username, String pwd)
+	public void addNewUser(String username, String name, String pwd, String role)
 	{
 		loginData.put(username, pwd);
+		users.put(username, role);
+		if(role.equals("doctor"))
+		{
+			doctors.put(username, new Doctor(username, name));
+		}
+		else if(role.equals("recep"))
+		{
+			receps.put(username, new Receptionist(username, name));
+		}
+		else if(role.equals("patient"))
+		{
+			patients.put(username, new Patient(username, name));
+		}
 	}
 
+
+	//Function for deleting the user from the system
+	public void deleteUser(String username)
+	{
+		String role = users.get(username);
+		loginData.remove(username);
+		users.remove(username);
+		if(role.equals("patient"))
+		{
+			patients.remove(username);
+		}
+		else if(role.equals("doctor"))
+		{
+			doctors.remove(username);
+		}
+		else if(role.equals("recep"))
+		{
+			receps.remove(username);
+		}
+	}
 
 	//To verify is the user exists and the to validate the password if user exists
 	//@Requires("!username.isNullOrEmpty()" && "!pwd.isNullOrEmpty()" && "loginData!=null")s
@@ -65,6 +117,57 @@ public class Login {
 		else
 		{
 			return false;
+		}
+	}
+
+	public Boolean verifyUserName(String username)
+	{
+		if(loginData.containsKey(username))
+		{
+			return false;
+		}
+		else
+		{
+			if(username.length() < 8)
+			{
+				return false;
+			}
+			else if(username.contains(" "))  
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	}
+
+	public Boolean verifyName(String name)
+	{
+		if(name.length() == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	public Boolean verifyPassword(String pwd)
+	{
+		if(pwd.length() < 4)
+		{
+			return false;
+		}
+		else if(pwd.contains(" "))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
 		}
 	}
 }
--- Hospital_Management_Version2/src/views/AdminPage.java
+++ Hospital_Management_Version2/src/views/AdminPage.java
@@ -1,6 +1,7 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -10,27 +11,48 @@ import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import common.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import java.awt.Font;
 
-public class AdminPage extends JFrame {
+public class AdminPage extends JFrame 
+{
 
 	private JPanel contentPane;
-	private JButton btnLogout;
 	private Login loginInfo;
+	private JButton btnLogOut;
+	private JButton btnAddUser;
+	private JPanel pnlAddUser;
+	private JButton btnDltUser;
+	private JPanel pnlDltUsr;
+	private JLabel lblUsername;
+	private JLabel lblName;
+	private JLabel lblPwd;
+	private JTextField txtUsr;
+	private JTextField txtName;
+	private JTextField txtPwd;
+	private JRadioButton rdbtnRecep;
+	private JRadioButton rdbtnAdmin;
+	private JRadioButton rdbtnDoctor;
+	private JLabel lblWarningDisp;
+	private JButton btnVerifyAndAdd;
+	private JTextField txtSearch;
+	private JLabel lblSearch;
+	private JButton btnSearch;
+	private JLabel lblFound;
+	private JButton btnDelete;
 
-	/**
-	 * Launch the application.
-	 */
-	
-
-	/**
-	 * Create the frame.
-	 */
-	public AdminPage(Login lg) {
+	public AdminPage(Login lg) 
+	{
 		loginInfo = lg;
 		initComponents();
 		createEvents();
 	}
-	
+
 	private void initComponents()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,22 +61,204 @@ public class AdminPage extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		btnLogout = new JButton("Logout");
-		btnLogout.setBounds(389, 332, 89, 23);
-		contentPane.add(btnLogout);
-	}
-	
-	private void createEvents()
-	{
-		btnLogout.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				LoginPage login = new LoginPage(loginInfo);
-				login.setVisible(true);
-				dispose();
-			}
-			
-		});
+
+		pnlDltUsr = new JPanel();
+		pnlDltUsr.setVisible(false);
+		pnlDltUsr.setBounds(160, 11, 318, 310);
+		contentPane.add(pnlDltUsr);
+		pnlDltUsr.setLayout(null);
+
+		txtSearch = new JTextField();
+		txtSearch.setText("");
+		txtSearch.setBounds(10, 11, 201, 20);
+		pnlDltUsr.add(txtSearch);
+		txtSearch.setColumns(10);
+
+		lblSearch = new JLabel("Enter username to delete");
+		lblSearch.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblSearch.setBounds(10, 42, 201, 20);
+		pnlDltUsr.add(lblSearch);
+
+		btnSearch = new JButton("Search");
+		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnSearch.setBounds(221, 10, 89, 23);
+		pnlDltUsr.add(btnSearch);
+
+		lblFound = new JLabel("");
+		lblFound.setBounds(10, 75, 269, 38);
+		pnlDltUsr.add(lblFound);
+
+		btnDelete = new JButton("Delete");
+		btnDelete.setVisible(false);
+		btnDelete.setBounds(10, 140, 89, 23);
+		pnlDltUsr.add(btnDelete);
+
+		btnLogOut = new JButton("Log Out");
+
+		btnLogOut.setBounds(389, 332, 89, 23);
+		contentPane.add(btnLogOut);
+
+		JPanel pnlAdminCtrl = new JPanel();
+		pnlAdminCtrl.setBounds(10, 11, 140, 344);
+		contentPane.add(pnlAdminCtrl);
+		pnlAdminCtrl.setLayout(null);
+
+		btnAddUser = new JButton("Add User");
+		btnAddUser.setBounds(10, 97, 120, 38);
+		pnlAdminCtrl.add(btnAddUser);
+
+		btnDltUser = new JButton("Delete user");
+		btnDltUser.setBounds(10, 191, 120, 38);
+		pnlAdminCtrl.add(btnDltUser);
+
+		pnlAddUser = new JPanel();
+		pnlAddUser.setVisible(false);
+		pnlAddUser.setBounds(160, 11, 318, 310);
+		contentPane.add(pnlAddUser);
+		pnlAddUser.setLayout(null);
+
+		lblUsername = new JLabel("User name");
+		lblUsername.setBounds(20, 11, 89, 30);
+		pnlAddUser.add(lblUsername);
+
+		lblName = new JLabel("Name");
+		lblName.setBounds(20, 79, 89, 30);
+		pnlAddUser.add(lblName);
+
+		lblPwd = new JLabel("Password");
+		lblPwd.setBounds(20, 146, 89, 30);
+		pnlAddUser.add(lblPwd);
+
+		txtUsr = new JTextField();
+		txtUsr.setBounds(20, 34, 240, 20);
+		pnlAddUser.add(txtUsr);
+		txtUsr.setColumns(10);
+
+		txtName = new JTextField();
+		txtName.setBounds(20, 102, 240, 20);
+		pnlAddUser.add(txtName);
+		txtName.setColumns(10);
+
+		txtPwd = new JTextField();
+		txtPwd.setBounds(20, 169, 240, 20);
+		pnlAddUser.add(txtPwd);
+		txtPwd.setColumns(10);
+
+		rdbtnDoctor = new JRadioButton("doctor");
+		rdbtnDoctor.setBounds(20, 209, 71, 23);
+		pnlAddUser.add(rdbtnDoctor);
+
+		rdbtnAdmin = new JRadioButton("admin");
+		rdbtnAdmin.setBounds(119, 209, 78, 23);
+		pnlAddUser.add(rdbtnAdmin);
+
+		rdbtnRecep = new JRadioButton("receptionist");
+		rdbtnRecep.setBounds(199, 209, 109, 23);
+		pnlAddUser.add(rdbtnRecep);
+
+		lblWarningDisp = new JLabel("");
+		lblWarningDisp.setBounds(20, 239, 288, 14);
+		pnlAddUser.add(lblWarningDisp);
+
+		btnVerifyAndAdd = new JButton("Add");
+		btnVerifyAndAdd.setBounds(229, 276, 89, 23);
+		pnlAddUser.add(btnVerifyAndAdd);
 	}
 
+	private void createEvents()
+	{
+		//When Login is pressed
+		btnLogOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LoginPage loginframe = new LoginPage(loginInfo);
+				loginframe.setVisible(true);
+				dispose();
+			}
+		});
+
+		//When Add user button is pressed
+		btnAddUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pnlAddUser.setVisible(true);	
+				pnlDltUsr.setVisible(false);
+			}
+		});
+
+		//When someone clicks the Add button to finally add the user
+		btnVerifyAndAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				String username = txtUsr.getText();
+				String name = txtName.getText();
+				String pwd = txtPwd.getText();
+				String role = "";
+				if(loginInfo.verifyUserName(username) && loginInfo.verifyName(name) && loginInfo.verifyPassword(pwd))
+				{
+					if(rdbtnDoctor.isSelected())
+					{
+						role = "doctor";
+					}
+					else if(rdbtnAdmin.isSelected())
+					{
+						role = "admin";
+					}
+					else if(rdbtnRecep.isSelected())
+					{
+						role = "recep";
+					}
+
+					if(!role.isEmpty())
+					{
+						loginInfo.addNewUser(username, name, pwd, role);
+						lblWarningDisp.setVisible(true);
+						lblWarningDisp.setText("User added succesfully");
+						lblWarningDisp.setForeground(Color.GREEN);
+					}
+				}
+				else
+				{
+					lblWarningDisp.setText("Invalid data or username present");
+					lblWarningDisp.setVisible(true);
+					lblWarningDisp.setForeground(Color.RED);
+
+				}
+			}
+		});
+
+
+		//When the delete button is clicked
+		btnDltUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pnlAddUser.setVisible(false);
+				pnlDltUsr.setVisible(true);
+			}
+		});
+
+		//When search button is clicked
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = txtSearch.getText();
+				if(username.equals("admin"))
+				{
+
+				}
+				else 
+				{
+					if(loginInfo.getLoginData().containsKey(username))
+					{
+						String role = loginInfo.getLoginData().get(username);
+						String found = "User " + username+ " found " + "\nUser type: " + role;
+						lblFound.setText(found);
+						btnDelete.setVisible(true);
+						lblFound.setForeground(Color.GREEN);
+					}
+					else
+					{
+						lblFound.setText("User name not found \nPlease type another username");
+						lblFound.setForeground(Color.RED);
+					}
+				}
+			}
+		});
+	}
 }
